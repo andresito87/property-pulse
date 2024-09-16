0bf5e1b59448b10d6e1c202064585eed01f524c5
Andres <andrespodadera87@gmail.com>
2024-09-16 22:31:06 +0200

    adding routing links

---
+++ app/properties/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const PropertyHomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">PropertyHomePage</h1>
+      <Link href="/">Back Home</Link>
+    </div>
+  );
+};
+
+export default PropertyHomePage;
